ae649e6e29ba3db87ad98b56d1fcf5962539cf2d
mishaNiv <misha.nivota@outlook.com>
2025-04-24 19:03:59 -0700

    Building predict function using model (this is what will take image and check with model whether fake or not) - not sure it works D:

---
+++ predict.py
@@ -0,0 +1,21 @@
+from keras.models import load_model as keras_load_model # type: ignore
+from PIL import Image
+import numpy as np
+import io
+from mesonet_model.classifiers import Meso4
+
+classifier = Meso4()
+classifier.load('weights/Meso4_DF.h5')
+
+def predict_image(file_img, model):
+    img = Image.open(file_img).convert('RGB').resize((256, 256))
+    img_arr = (np.asarray(img) / 255.0).reshape((1, 256, 256, 3))
+
+    pred = classifier.predict(img_arr)[0][0]
+
+    print(pred)
+
+    if pred > 0.5:
+        return { "label": "fake" }
+    else:
+        return { "label" : "real" }
